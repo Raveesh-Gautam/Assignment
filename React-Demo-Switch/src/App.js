bf4cf90194dae4e8b0c36938eb2df39ba05ac6dc
Raveesh-Gautam <92278944+Raveesh-Gautam@users.noreply.github.com>
2022-12-29 20:29:03 +0530

    initial commit

---
+++ React-Demo-Switch/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './asssets/style.css'
+import SwitchButton from './components/Switch';
+function App() {
+  return (
+    <div className="parentDiv">
+        <SwitchButton />
+    </div>
+  );
+}
+
+export default App;
